0b748f13db390389b9e0fc12d728c85ed86ff37a
Aswathi Sunil <aswathisunil238@gmail.com>
2026-06-05 17:15:19 +0530

    Fixed media serving in production

--- getellaam_project/settings.py
+++ getellaam_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#4-027r)-zy3m&(1&)0ss!4yee5u_s(ezr4#fjhtg0_q0qbycp'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -139,7 +140,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = 'demogetellaam@gmail.com'
-EMAIL_HOST_PASSWORD = 'geofayiaofhmzkso'
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
@@ -152,6 +153,5 @@ LOGIN_REDIRECT_URL = '/dashboard/'
 LOGOUT_REDIRECT_URL = '/'
 
 RECAPTCHA_SITE_KEY = "6LfoHdUsAAAAAFPQk0ntSFe1-PIe91Nz1C69CWgy"
-import os
 
 RECAPTCHA_SECRET_KEY = os.environ.get("RECAPTCHA_SECRET_KEY")
